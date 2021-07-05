5d09b2963460a4a5ed3f46797c3a0b7ef4d6cf34
jmpfbmx <jmpf.bmx@gmail.com>
2021-07-05 16:06:54 +0200

    redwood: Initial device tree
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I7ae7346f6e9982462e6de36f2346ad335ccdcab1

---
+++ BoardConfig.mk
@@ -0,0 +1,34 @@
+#
+# Copyright 2020 The LineageOS Project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/sony/mt6757-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/sony/redwood
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := XPERIA_XA1_ULTRA,redwood,xperia_xa1_ultra
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_redwood_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/etc/twrp.fstab
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+BOARD_HAS_NO_SELECT_BUTTON := true
---
+++ lineage.mk
@@ -0,0 +1,48 @@
+#
+# Copyright 2020 The LineageOS Project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from redwood device
+$(call inherit-product, device/sony/redwood/device.mk)
+
+# Inherit from common device
+$(call inherit-product, device/sony/mt6757-common/mt6757.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit languages full.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := SONY
+PRODUCT_DEVICE := redwood
+PRODUCT_NAME := lineage_redwood
+PRODUCT_BRAND := SONY
+PRODUCT_MODEL := XPERIA XA1 Ultra
+PRODUCT_MANUFACTURER := SONY
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=redwood \
+   PRIVATE_BUILD_DESC="G3221-user 8.0.0 48.1.A.2.112 1877064199 release-keys"
+
+BUILD_FINGERPRINT := "Sony/G3221/G3221:8.0.0/48.1.A.2.112/1877064199:user/release-keys"
